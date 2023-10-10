5281077224c1312392bebc5b993b35ed6bdfd9b1
marcos-messias <marcosmessias350@gmail.com>
2023-10-10 14:56:19 -0300

    Trabalho

---
+++ Mapa/js/script.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([-22.120495387889097, -51.40559058389142], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
